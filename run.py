3ff32b821db9e98acdd14960134842069a230a77
neil314159 <neil@neilboland.com>
2021-12-28 16:22:24 +0000

    Update stock data functions improved

--- run.py
+++ run.py
@@ -53,7 +53,7 @@ def validate_data(values):
 
 
 def update_worksheet(data, worksheet):
-    print(f'UPdating {worksheet} worksheet . . .\n')
+    print(f'Updating {worksheet} worksheet . . .\n')
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n")
@@ -89,6 +89,19 @@ def get_last_five_entries_sales():
 
     return columns
 
+def calculate_stock_data(data):
+    print("Calculating stock data . . .\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average *1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 def main():
     """
     Main functions
@@ -98,7 +111,9 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_five_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print('Welcome to Love Sandiwches automation')
-#main()
-get_last_five_entries_sales()
+main()
